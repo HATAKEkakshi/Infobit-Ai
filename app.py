bcbc30232792d45cc8dc1c8f2001cd5e20c84ea2
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-06-14 09:27:26 +0530

    gemini

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
-##conversional  q&a chatbot
 import streamlit as st
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
-from langchain_community.chat_models import ChatOpenAI
-
-##streamlit ui
-st.set_page_config(page_title="Conversional Q&A ChatBot")
-st.header("Hey , Lets Chat")
-
-from dotenv import load_dotenv # type: ignore
-load_dotenv()
 import os
-chat=ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),temperature=0.5)
-if 'flowmessage' not in st.session_state:
-    st.session_state['flowmessage']=[
-        SystemMessage(content="You are a Ai asstiant for general stuff give answer as accurate as possible")
-        ]
-##function to load opeai 
-def get_openai_response(question):
-    st.session_state['flowmessage'].append(HumanMessage(content=question))
-    answer=chat(st.session_state['flowmessage'])
-    st.session_state['flowmessage'].append(AIMessage(content=answer.content))
-    return answer
+import google.generativeai as genai
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv # type: ignore
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+load_dotenv()
+#function to load openai model and get response 
+def get_gemini_response(question):
+    llm = GoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    response=llm(question)
+    return response
+##initialize our streamlit app
 
+st.set_page_config(page_title="Q$A Demo")
 
+st.header("infobit.py") 
 input=st.text_input("Input : ",key="input")
-response=get_openai_response(input)
+response=get_gemini_response(input)
 submit=st.button("Ask the question")
-#if ask button is clicked
+
+##if submit button is clicked
 
 if submit:
-    st.subheader("The Response is")
-    st.write(response)
+    st.subheader("The Response is ")
+    st.write(response) # type: ignore
